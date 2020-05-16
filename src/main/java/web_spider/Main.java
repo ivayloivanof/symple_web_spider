c6a2ad8529a9ad8b5aee3bfcc3a4e4b969276bfa
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2020-05-16 18:29:58 +0300

    create main and fast test

---
+++ src/main/java/web_spider/Main.java
@@ -0,0 +1,12 @@
+package web_spider;
+
+import web_spider.parser.PageReader;
+
+public class Main {
+
+	public static void main(String[] args) {
+		PageReader pageReader = new PageReader("https://dir.bg/");
+		System.out.println();
+	}
+
+}
