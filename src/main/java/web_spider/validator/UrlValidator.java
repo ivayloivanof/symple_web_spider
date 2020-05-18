76eb55d8255962643fc63af8c0afe6b29df8920e
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2020-05-18 06:12:17 +0300

    change regex pattern for url address

--- src/main/java/web_spider/validator/UrlValidator.java
+++ src/main/java/web_spider/validator/UrlValidator.java
@@ -7,7 +7,7 @@ public class UrlValidator {
 	
 	public static boolean isValid(String url) {
 		//Pattern to check if this is a valid URL address
-		Pattern p = Pattern.compile("^(http://|https://)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[a-z]{3}.?([a-z]+)?$");
+		Pattern p = Pattern.compile("(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})");
 		Matcher matcher = p.matcher(url);
 		
 		return matcher.matches();
