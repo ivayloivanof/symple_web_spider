88448aa8ea8d6213d8e431bb62c33ee54bd20de6
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2020-05-18 07:11:48 +0300

    check all results

--- src/main/java/web_spider/Main.java
+++ src/main/java/web_spider/Main.java
@@ -1,11 +1,29 @@
 package web_spider;
 
+import java.util.HashSet;
+
 import web_spider.parser.PageReader;
 
 public class Main {
 
 	public static void main(String[] args) {
-		PageReader pageReader = new PageReader("https://dir.bg/");
+		HashSet<PageReader> pages = new HashSet<PageReader>();
+		PageReader pageReader = new PageReader("https://emailblastgenerator.blogspot.com/2017/07/email-list-txt-download.html");
+		pages.add(pageReader);
+		
+		for (String url : pageReader.getPageLinks()) {
+			pages.add(new PageReader(url));
+		}
+		
+		for (PageReader pReader : pages) {
+			for (String link : pReader.getPageLinks()) {
+				System.out.println(link);
+			}
+			for (String emal : pReader.getPageEmails()) {
+				System.out.println(emal);
+			}
+		}
+		
 		System.out.println();
 	}
 
